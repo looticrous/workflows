b653d9378a90a4b08c1695563cc4bbcff039fa0d
David Blanchard <David.Blanchard@watco.com>
2025-09-29 17:25:35 -0500

    Refactor workflow by removing WIP steps and adding Nx Get Builds action for improved build management

---
+++ .github/actions/nx-get-builds/nx-get-builds.ps1
@@ -0,0 +1,18 @@
+[cmdletbinding()]
+param (
+  [Parameter(Mandatory = $false)] 
+  [string]
+  $projects, 
+  [Parameter(Mandatory = $true)]
+  [string]
+  $base_ref
+)
+
+if (-not [string]::IsNullOrEmpty($projects)) {
+  Write-Host "Using provided projects: $projects)"
+}
+else {
+  Write-Host "Using nx affected with base: $base_ref"
+  $projects = (npx nx show projects --affected --base=$base_ref --with-target=container-build --json | ConvertFrom-Json)
+}
+"projects=$($projects -join ',')" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
