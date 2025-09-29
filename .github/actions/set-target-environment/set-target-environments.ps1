f7dfb73cd56b44f4d225841f003af935e7c27a7a
David Blanchard <David.Blanchard@watco.com>
2025-09-29 16:50:11 -0500

    Refactor target environment handling by replacing script with action and updating print step in composer.yml

---
+++ .github/actions/set-target-environment/set-target-environments.ps1
@@ -0,0 +1,16 @@
+[cmdletbinding()]
+param (
+  [Parameter(Mandatory = $true)]
+  [string]$event_name, 
+  [Parameter(Mandatory = $false)]
+  [string]$target_environments
+)
+
+$in = $param1 | ConvertFrom-Json
+if ($in.event_name -eq 'workflow_dispatch') {
+  $environments = $target_environments.Split(",") | ForEach-Object { $_.Trim() }
+}
+else {
+  $environments = @("Development", "QA", "UAT") 
+}
+"target_environments=$($environments -join ',')" | Out-File -FilePath $env:GITHUB_OUTPUT -Encoding utf8 -Append
